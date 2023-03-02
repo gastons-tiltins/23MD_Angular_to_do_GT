fced2341c783824338eeb0618de3a3422f297303
Gastons Tiltins <gastons.tiltins@gmail.com>
2023-03-02 15:39:08 +0200

    1st q git commit -m 1st

---
+++ src/app/app-types.ts
@@ -0,0 +1,6 @@
+export interface ListItems {
+    id: number;
+    name: string;
+    price: number;
+    toggle: boolean;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { ListItems } from './app-types';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'myApp';
+    title = 'Todo';
+
+    listItems: ListItems[] = [
+        {
+            id: 1,
+            name: 'Web Development',
+            price: 300,
+            toggle: true,
+        },
+        {
+            id: 2,
+            name: 'Design',
+            price: 400,
+            toggle: true,
+        },
+        {
+            id: 3,
+            name: 'Integration',
+            price: 250,
+            toggle: true,
+        },
+        {
+            id: 4,
+            name: 'Training',
+            price: 220,
+            toggle: true,
+        },
+    ];
+
+    result: number = 0;
+
+    handleClick(id: number, price: number, toggle: boolean) {
+        if (toggle) {
+            this.result += price;
+            let item = this.listItems.filter(item => id === item.id);
+            item[0].toggle = !toggle;
+        } else {
+            this.result -= price;
+            let item = this.listItems.filter(item => id === item.id);
+            item[0].toggle = !toggle;
+        }
+    }
 }
